6d0f7553806188847564031a83f084cd43d27706
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-05-27 20:38:05 -0400

    login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,19 +2,27 @@ import { login, signup } from "./actions";
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        className="text-black"
-        required
-      />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex justify-center content-center border">
+      <form className="flex justify-center flex-col">
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          className="text-black"
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          className="text-black"
+          id="password"
+          name="password"
+          type="password"
+          required
+        />
+        <button formAction={login}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form>
+    </div>
   );
 }
