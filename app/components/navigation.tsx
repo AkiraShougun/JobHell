39207244c735f0c9d6d1d05bf55ec76bd179cd15
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-06-01 23:51:57 -0400

    buttons

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,7 +1,9 @@
 const Navigation = () => {
   return (
     <section className=" border-r border-r-slate-500 h-screen border-rad">
-      Navigation
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
     </section>
   );
 };
