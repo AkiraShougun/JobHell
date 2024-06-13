5efe6d1bb209b035a60104f51e887e7d5c381446
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-06-13 18:33:24 -0400

    tailwind

--- app/components/addJob.tsx
+++ app/components/addJob.tsx
@@ -12,7 +12,7 @@ const AddJob = () => {
     <>
       <button
         onClick={() => setOpenModal(true)}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded"
       >
         Add A Job
       </button>
